ac041ff463bfaaeeea59cf8e3844b8661f121848
Taci Shlosberg <tshlosberg@gmail.com>
2020-11-20 01:43:54 -0800

    Redirect

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,32 +1,4 @@
-import React, { Component, useState } from "react";
-import Button from "./Button";
-
-
-// export default function About() {
-
-
-//     const [clicked, setClick] = useState(false);
-//     const handleGetInTouchClick = () => setClick(!clicked);
-
-//     return (
-//         <div className="profile">
-//             {/* <img className="bio-img" src={elephant} alt="south african elephant" /> */}
-//             <p className="bio-intro"> 
-//                 Hi there! My name is
-//                 <p className="bio-name"> Taci Shlosberg.</p>
-//                 {/* <h4 className="p2"> I love building things for the web. </h4> */}
-//                 <p className="bio-paragraph">
-//                     I'm a Full-Stack Web Developer based in <br/>
-//                     San Francisco, CA and a UI/UX enthusiast.
-//                 </p>
-//             </p>
-//             <Button 
-//                 text="Get in Touch" 
-//                 onClick={handleGetInTouchClick}
-//             />
-//         </div>
-//     )
-// }
+import React, { Component, Redirect } from "react";
 
 
 class About extends Component {
@@ -39,8 +11,10 @@ class About extends Component {
         this.setState({
             clicked: true
         });
-        window.location.href = "http://localhost:3000/contact";
-        console.log('clicked');
+        <Redirect to="/contact" />
+        // this.props.history.push("/contact")
+        // window.location.href = "http://localhost:3000/contact";
+        // console.log('clicked');
     }
 
 
@@ -59,7 +33,6 @@ class About extends Component {
                         </p>
                     </p>
                     <button className="button" onClick={this.handleGetInTouchClick}> Get in Touch </button>
-                    {/* <Button text="Get in Touch" onClick={this.handleGetInTouchClick}/> */}
              </div>
          </div>
         )
