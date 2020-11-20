ec8693556f7aca077b72079a0728af2f976fb14d
Taci Shlosberg <tshlosberg@gmail.com>
2020-11-19 20:09:10 -0800

    Contact form working

---
+++ my-portfolio-server/index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/form", async (request, response) => {
+
+    const htmlEmail = `
+        <p>New message from your Porfolio<p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>From: ${request.body.name}</li>
+            <li>Email: ${request.body.email}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${request.body.message}</p>
+    `
+
+    let transporter = await nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASSWORD
+        },
+    });
+ 
+    let mail = {
+        from: request.body.email,
+        to: process.env.GMAIL_USER, 
+        subject: "You have a new message!", 
+        text: request.body.message, 
+        html: htmlEmail, 
+    };
+
+    transporter.sendMail(mail, (error, data) => {
+        if(error) {
+            console.log('error', error);
+            response.json({ message: "An error occured, please try again."})
+        } else {
+            console.log("Message sent");
+            response.json({ message: `Message sent: %s, ${data.message}` })
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
