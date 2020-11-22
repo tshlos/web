0268f359af63b1d630cf89a391f8ca728b924fc4
Taci Shlosberg <tshlosberg@gmail.com>
2020-11-22 15:53:12 -0800

    Updated cors

--- my-portfolio-server/index.js
+++ my-portfolio-server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://taci-portfolio.herokuapp.com/api/form");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
